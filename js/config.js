c1486c7f971b20e888f3aa0322ca521f2fb8d4bb
leo <chichali374@gmail.com>
2024-12-08 09:46:17 +0800

    建立設定檔案

---
+++ js/config.js
@@ -0,0 +1,27 @@
+const api_path = "leo533";
+const token = "XuK0KP4RkXR6ywFwzAvx7jPYBZg1";
+const acesstoken = {
+  headers: {
+    'Authorization': token
+  }
+}
+// menu 切換
+let menuOpenBtn = document.querySelector('.menuToggle');
+let linkBtn = document.querySelectorAll('.topBar-menu a');
+let menu = document.querySelector('.topBar-menu');
+menuOpenBtn.addEventListener('click', menuToggle);
+
+linkBtn.forEach((item) => {
+  item.addEventListener('click', closeMenu);
+})
+
+function menuToggle() {
+  if (menu.classList.contains('openMenu')) {
+    menu.classList.remove('openMenu');
+  } else {
+    menu.classList.add('openMenu');
+  }
+}
+function closeMenu() {
+  menu.classList.remove('openMenu');
+}
